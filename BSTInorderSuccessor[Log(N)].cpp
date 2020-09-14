0a7c901f829254394b0dc3f484488db63c2e5bff
discoversaurabh <discover.saurabh@gmail.com>
2020-09-14 19:30:42 +0530

    Another Code (need to check which one is better)

--- BSTInorderSuccessor[Log(N)].cpp
+++ BSTInorderSuccessor[Log(N)].cpp
@@ -1,6 +1,6 @@
 /* INORDER SUCCESSOR */
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -8,27 +8,32 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-TreeNode* Solution::getSuccessor(TreeNode* A, int B) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    if(!A) return NULL;
-    
-    TreeNode* potSuc = NULL;
-    
-    while(A)
-    {
-        if(A->val > B)
-        {
-            if(!potSuc)
-                potSuc = A;
-            else
-                potSuc = (potSuc->val < A->val)?potSuc : A;
-            A = A->left;
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if(!root || !p) return NULL;
+        TreeNode* tmp = p->right;
+        while(tmp){
+            if(!tmp->left) return tmp;
+            tmp = tmp->left;
         }
-        else
-            A = A->right;
+        
+        //now successor is in p's parent
+        tmp = root;
+        TreeNode* successor = NULL;
+        
+        while(tmp){
+            if(p->val == tmp->val) return successor;
+            
+            else if(p->val < tmp->val){
+                if(!successor || successor->val > tmp->val){
+                    successor = tmp;
+                }
+                tmp = tmp->left;
+            }else {
+                tmp = tmp->right;
+            }
+        }
+        return successor;
     }
-    return potSuc;
-}
+};
